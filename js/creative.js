c5536713ee448e418aad7fac742eb836f0e9efdf
Cassidy Hsieh <cassidy.hsieh@MBP121-044.local>
2016-07-21 16:59:35 -0700

    new error image, removed bootstrap shit

--- js/creative.js
+++ js/creative.js
@@ -11,22 +11,22 @@
     });
 
     // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
+    // $('body').scrollspy({
+    //     target: '.navbar-fixed-top',
+    //     offset: 51
+    // });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
+    // $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+    //     $('.navbar-toggle:visible').click();
+    // });
 
     // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+    // $('#mainNav').affix({
+    //     offset: {
+    //         top: 100
+    //     }
+    // })
 
     // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();
