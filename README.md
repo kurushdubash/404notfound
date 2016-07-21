8a1767ed07f812ddc5a6f31ea8c95fc752f8f1d1
Cassidy Hsieh <cassidy.hsieh@MBP121-044.local>
2016-07-21 13:55:40 -0700

    removed irrelevant files

--- README.md
+++
@@ -1,4 +0,0 @@
-#####
-A makeover for Tube's 404 page.
-
-Authors: Kurush Dubash, Cassidy Hsieh, Richard Xu, Alton Lin, Jordan Milner
--- server.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(process.env.PORT || 8080, function(){
-    console.log('Server running on 8080...');
-});
