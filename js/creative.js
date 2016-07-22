c8a047d3ffa8572c0b025765ce69d0aa86539789
Cassidy Hsieh <cassidy.hsieh@stats-roll-validator01.qa.us-east-1d.private>
2016-07-22 09:57:40 -0700

    content formatting done

--- js/creative.js
+++ js/creative.js
@@ -60,7 +60,8 @@ function openCity(evt, list1, list2, list3, list4) {
     document.getElementById("divpeep").innerHTML = "<img src=" + list2.url + " class='people-image'>";
 
     document.getElementById("h3place").innerHTML = list3.name;
-    document.getElementById("pplace").innerHTML = list3.description;
+    document.getElementById("pplace").innerHTML = list3.location;
+    document.getElementById("p2place").innerHTML = list3.description;
     document.getElementById("divplace").innerHTML = "<img src=" + list3.url + " class='place-image'>";
 
     document.getElementById("h3puppies").innerHTML = list4.name;
@@ -72,53 +73,56 @@ function openCity(evt, list1, list2, list3, list4) {
 
 var person1 = {
     name: "Kurush",
-    department: "PTV",
+    department: "PTV Team",
     description: "I like long bike rides to TubeMogul, rockclimbing without ropes, and watching my stock investments fail... Oh wait is this not for tinder?",
     url: "http://i.imgur.com/IjCLh9Y.jpg"
 }
 
 var person2 = {
     name: "Cassidy",
-    department: "UI",
+    department: "UI Team",
     description: "I am the only female intern, the odds are good but the goods are odd.",
     url: "http://i.imgur.com/MtKSMXC.jpg"
 }
 
 var person3 = {
     name: "Jordan",
-    department: "UI",
+    department: "UI Team",
     description: "Did you know that Raleigh, North Carolina is the Silicon Valley of the East Coast?",
     url: "http://i.imgur.com/xOUi2b8.jpg"
 }
 
 var person4 = {
     name: "Richard",
-    department: "RTB",
+    department: "RTB Team",
     description: "You can call me by my nickname: Lil Dicky Shoe.",
     url: "http://i.imgur.com/DEcGdDK.jpg"
 }
 
 var person5 = {
     name: "Alton",
-    department: "STATS",
+    department: "STATS Team",
     description: "I'm a rising junior studying EECS at UC Berkeley and a Software Engineering Intern on the STATS team.",
     url: "http://i.imgur.com/AOngfTP.jpg"
 }
 
 var place1 = {
-    name: "Emeryville, California",
+    name: "Emeryville",
+    location: "California",
     description: "Eight years ago TubeMogul was a tiny startup located in the basement of a Berkeley hotel. Currently, TubeMogul is based in Emeryville, occupying Pixar’s previous buildings where many of their original animated films were made!",
     url: "http://i.imgur.com/2U5lfgx.jpg",
 }
 
 var place2 = {
-    name: "London, United Kingdom",
+    name: "London",
+    location: "United Kingdom",
     description: "Many argue that the birthplace of our industry can be found in London - all thanks to the ‘father of modern advertising’, Thomas J. Barratt who created the first-ever advertising slogan – so it’s no wonder we set up shop where it all began.",
     url: "http://i.imgur.com/q5RNyut.jpg",
 }
 
 var place3 = {
-    name: "New York, New York",
+    name: "New York",
+    location: "New York",
     description: "Called many things: “The Big Apple, “Gotham City, “The City that Never Sleeps”. To NY TubeMogulers, we call NYC home. Many of us are transplants from other cities; however, as Tom Wolf accurately said “One belongs to New York instantly, one belongs to it as much in five minutes as in five years.”",
     url: "http://i.imgur.com/zZK7vE1.jpg",
 }
